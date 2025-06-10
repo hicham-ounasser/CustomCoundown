23bd13add31fb1462136b49056c1f37df79b60b4
Hicham Ounasser <ounasser.hicham@gmail.com>
2025-06-10 16:52:20 +0100

    I added and styled the countdown component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import VideoBackground from "./components/VideoBackground";
 import CountdownContainer from "./components/CountdownContainer";
+import Countdown from "./components/Countdown";
 import "./App.css";
 
 function App() {
   return (
     <>
+      <Countdown />
+
       <VideoBackground />
-      <CountdownContainer />
     </>
   );
 }
---
+++ src/components/Countdown.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Countdown() {
+  return (
+    <div className="countdown">
+      <h1>Countdown Tilte Here</h1>
+      <ul>
+        <li>
+          3<span>Days</span>
+        </li>
+        <li>
+          6<span>Hours</span>
+        </li>
+        <li>
+          58 <span>Minutess</span>
+        </li>
+        <li>
+          31
+          <span>Seconds</span>
+        </li>
+      </ul>
+
+      <button>Reset</button>
+    </div>
+  );
+}
+
+export default Countdown;
